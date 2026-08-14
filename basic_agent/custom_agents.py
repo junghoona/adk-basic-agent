6acdc030361e4dd2ce1fd4d9dd109077290c2429
junghoona <127.an.john@gmail.com>
2026-08-14 16:18:28 -0700

    [LAB-2B] Added wrapper function to call native Google Search

---
+++ basic_agent/custom_agents.py
@@ -0,0 +1,16 @@
+"""AI agent (Python wrapper function) to call native Google Search
+"""
+
+
+from google.adk.agents import Agent
+from google.adk.tools import google_search
+
+# Create an agent with google search tool as a search specialist
+
+google_search_agent = Agent(
+    model='gemini-3.5-flash',
+    name='google_search_agent',
+    description='A search agent that uses google search to get latest information...',
+    instruction='Use google search to answer user questions about real-time info.',
+    tools=[google_search],
+)
